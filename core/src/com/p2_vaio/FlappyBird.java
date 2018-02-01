37167a3a1ecfbafa8a98162e23e8cce0c0a51f29
PrafullPandey <p2.prafull@gmail.com>
2018-02-01 07:26:48 +0530

    setting up the bird movement on tap of screen

--- core/src/com/p2_vaio/FlappyBird.java
+++ core/src/com/p2_vaio/FlappyBird.java
@@ -14,6 +14,12 @@ public class FlappyBird extends ApplicationAdapter {
 	int flap_state=0;
 	float accumulator = 0f;
 
+	float birdY=0;
+	float velocity = 0;
+	float gravity =2;
+
+	int gamestate=0;
+
 	//runs one time on run
 	@Override
 	public void create () {
@@ -23,21 +29,41 @@ public class FlappyBird extends ApplicationAdapter {
 		background= new Texture("bg.png");
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
+
+		birdY=Gdx.graphics.getHeight()/2-birds[flap_state].getHeight()/2;
 	}
 
 	//happens continously as the app run
 	@Override
 	public void render () {
 
-		batch.begin();
-		//repeatedlt displaying it on screen
-		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(birds[flap_state],Gdx.graphics.getWidth()/2-birds[flap_state].getWidth()/2,Gdx.graphics.getHeight()/2-birds[flap_state].getHeight()/2);
+		if(gamestate!=0) {
 
+			//detecting the tap
+			if(Gdx.input.justTouched()){
+				//birdY++;
+				velocity=-20;
+			}
+
+			if(birdY>0||velocity<0){
+
+				velocity+=gravity;
+				birdY-=velocity;
+			}
+
+
+
+		}else{
+			if(Gdx.input.justTouched()){
+
+				//birdY++;
+				gamestate=1;
+			}
+		}
 
 		float dt = Gdx.graphics.getDeltaTime(); //dt is the number of seconds that have passed in between each frame
 		accumulator += dt;
-		if(accumulator > 0.15)//one second has passed
+		if (accumulator > 0.15)//one second has passed
 		{
 			if (flap_state == 0)
 				flap_state = 1;
@@ -45,8 +71,14 @@ public class FlappyBird extends ApplicationAdapter {
 				flap_state = 0;
 			accumulator -= 0.15;
 		}
-
+		batch.begin();
+		//repeatedlt displaying it on screen
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birds[flap_state], Gdx.graphics.getWidth() / 2 - birds[flap_state].getWidth() / 2, birdY);
 		batch.end();
+
+
+
 
 	}
 	
